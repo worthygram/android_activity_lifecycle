8a00aaa822482353fd4c7bf2be36f95d6bc7ae91
peelu123 <raghuwanshipalash6@gmail.com>
2023-04-18 18:24:02 +0530

    added lifecycle activities functions in SecondActivity.kt fiie

--- app/src/main/java/com/anushka/activitylifecycledemo/SecondActivity.kt
+++ app/src/main/java/com/anushka/activitylifecycledemo/SecondActivity.kt
@@ -2,6 +2,7 @@ package com.anushka.activitylifecycledemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
@@ -12,5 +13,35 @@ class SecondActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.tvOffer)
         val message = "$userName ,you will get  free access to all the content for one month "
         textView.text = message
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MYTAG","SecondActivity : OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MYTAG","SecondActivity : OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MYTAG","SecondActivity : onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MYTAG","SecondActivity : onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MYTAG","SecondActivity : onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MYTAG","SecondActivity : onRestart")
     }
 }
